05bf1112d31a6afd324950e34e639c661286fafc
Olivier Grisel <olivier.grisel@ensta.org>
2014-02-14 14:07:53 -0800

    First commit with blocking rdd utilities

---
+++ README.md
@@ -0,0 +1,3 @@
+# PySpark / scikit-learn glue code
+
+Repo with experimental tools to show how to combine scikit-learn and PySpark.
---
+++ blocked_rdd.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pandas as pd
+
+
+def block_tuple(iterator, block_size=None):
+    i = 0
+    tuple_size = None
+    blocked_tuple = None
+    for tuple_i in iterator:
+        if blocked_tuple is None:
+            blocked_tuple = tuple([] for _ in range(len(tuple_i)))
+            
+        if block_size is not None and i > block_size:
+            yield tuple(np.array(x) for x in blocked_tuple)
+            blocked_tuple = tuple([] for _ in range(len(tuple_i)))
+            i = 0
+        for x_j, x in zip(tuple_i, blocked_tuple):
+            x.append(x_j)
+        i += 1
+    yield tuple(np.array(x) for x in blocked_tuple)
+
+
+def block_array(iterator, block_size=None):
+    i = 0
+    arrays = []
+    for a in iterator:
+        if block_size is not None and i > block_size:
+            yield np.array(arrays)
+            arrays = []
+        arrays.append(a)
+        i += 1
+    yield np.array(arrays)
+
+
+def block_dataframe(iterator, book_size=None):
+    i = 0
+    rows = []
+    for d in iterator:
+        if block_size is not None and i > block_size:
+            yield pd.DataFrame(rows)
+            rows = []
+        rows.append(d)
+        i += 1
+    yield pd.DataFrame(rows)
+
