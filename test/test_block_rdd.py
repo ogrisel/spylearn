c53fe3307e3d57403ff1bcb5efaa748f58a526b7
Olivier Grisel <olivier.grisel@ensta.org>
2014-02-15 10:35:55 -0800

    test block_size

--- test/test_block_rdd.py
+++ test/test_block_rdd.py
@@ -7,6 +7,7 @@ from common import SpylearnTestCase
 from spylearn.block_rdd import block_rdd
 
 from nose.tools import assert_equal
+from nose.tools import assert_true
 from numpy.testing import assert_array_almost_equal
 
 
@@ -46,6 +47,20 @@ class TestBlockRDD(TestUtils):
         blocks = blocked_data.collect()
         assert_equal(len(blocks), n_partitions)
         assert_equal(sum(len(b) for b in blocks),  n_samples)
+
+    def test_block_rdd_array_block_size(self):
+        n_partitions = 10
+        n_samples = 107
+        data = self.sc.parallelize([np.array([1]) for i in range(n_samples)],
+            n_partitions)
+
+        block_data_5 = block_rdd(data, block_size=5)
+        blocks = block_data_5.collect()
+        assert_true(all(len(b) <= 5 for b in blocks))
+
+        block_data_10 = block_rdd(data, block_size=10)
+        blocks = block_data_10.collect()
+        assert_true(all(len(b) <= 10 for b in blocks))
 
     def test_block_rdd_dict(self):
         pass
