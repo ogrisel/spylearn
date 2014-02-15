d966003002d11ffc642d23459c371c712a28254d
Olivier Grisel <olivier.grisel@ensta.org>
2014-02-15 10:30:09 -0800

    more tests for block_rdd

--- test/test_block_rdd.py
+++ test/test_block_rdd.py
@@ -5,6 +5,8 @@ import numpy as np
 from common import SpylearnTestCase
 
 from spylearn.block_rdd import block_rdd
+
+from nose.tools import assert_equal
 from numpy.testing import assert_array_almost_equal
 
 
@@ -24,9 +26,26 @@ class TestBlockRDD(TestUtils):
         pass
 
     def test_block_rdd_array(self):
-        data = self.sc.parallelize([np.array([1]) for i in range(0, 100)], 10)
+        n_partitions = 10
+        n_samples = 100
+        data = self.sc.parallelize([np.array([1]) for i in range(n_samples)],
+            n_partitions)
         blocked_data = block_rdd(data)
         assert_array_almost_equal(np.ones((10, 1)), blocked_data.first())
+        blocks = blocked_data.collect()
+        assert_equal(len(blocks), n_partitions)
+        assert_array_almost_equal(np.ones((10, 1)), blocks[-1])
+        assert_equal(sum(len(b) for b in blocks),  n_samples)
+
+        n_partitions = 17
+        data = self.sc.parallelize([np.array([1]) for i in range(n_samples)],
+            n_partitions)
+        blocked_data = block_rdd(data)
+        assert_array_almost_equal(np.ones((n_samples / n_partitions, 1)),
+                                  blocked_data.first())
+        blocks = blocked_data.collect()
+        assert_equal(len(blocks), n_partitions)
+        assert_equal(sum(len(b) for b in blocks),  n_samples)
 
     def test_block_rdd_dict(self):
         pass
