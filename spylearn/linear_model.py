3aaf52f4c9896c1004a373aeb64afc3b5013061e
Olivier Grisel <olivier.grisel@ensta.org>
2014-02-18 18:26:08 +0100

    work in progress cross validated linear models

--- spylearn/linear_model.py
+++ spylearn/linear_model.py
@@ -1,7 +1,9 @@
 """Parallel Linear Model training with partial_fit and averaging"""
 
+from sklearn.base import clone
 
-def _train(iterator, model, classes):
+
+def _train(iterator, model, classes, skip=None):
     for X, y in iterator:
         model.partial_fit(X, y, classes=classes)
     yield model, 1
@@ -22,3 +24,51 @@ def parallel_train(model, data, classes=None, n_iter=10):
         model.coef_ /= count
         model.intercept_ /= count
     return model
+
+
+def _train_cv(iterator, models, classes):
+    for i, (X, y) in enumerate(iterator):
+        for j, model in enumerate(models):
+            if i % len(models) != j:
+                model.partial_fit(X, y, classes=classes)
+    yield models, 1
+
+
+def _predict_cv(iterator, models):
+    for i, (X, y) in enumerate(iterator):
+        predictions = []
+        for j, model in enumerate(models):
+            if i % len(models) == j:
+                y_predicted = model.predict(X)
+                predictions.append((y, y_predicted))
+        yield tuple(predictions)
+
+
+def _models_sum(m_1, m_2):
+    models_1, count_1 = m_1
+    models_2, count_2 = m_2
+    for model_1, model_2 in zip(models_1, models_2):
+        model_1.coef_ += model_2.coef_
+        model_1.intercept_ += model_2.intercept_
+    return models_1, count_1 + count_2
+
+
+def _accuracy_sum(p_1, p_2):
+    pred_1, count_1 = p_1
+    pred_2, count_2 = p_2
+    diffs = [(true != pred)]
+
+
+def parallel_cross_validation(model, data, classes=None, n_folds=5, n_iter=10):
+    # train one model per fold
+    models = [clone(model) for i in range(n_folds)]
+    for i in range(n_iter):
+        train = lambda x: _train_cv(x, models, classes)
+        models, count = data.mapPartitions(train).reduce(_models_sum)
+        for model in models:
+            model.coef_ /= count
+            model.intercept_ /= count
+
+    # compute predictions for each model
+    predict = lambda x: _predict_cv(x, models)
+    accuracies = data.mapPartitions(predict).reduce(_accuracy_sum)
